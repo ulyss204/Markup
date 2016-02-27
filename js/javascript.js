6c04651c60f97d86a3952e1dfc01692b44fead6b
Andy <rym1112ov@gmail.com>
2016-02-27 12:13:40 +0200

    Add files

---
+++ js/javascript.js
@@ -0,0 +1,18 @@
+function toggleClass(element, className){
+    if (!element || !className){
+        return;
+    }
+    
+    var classString = element.className, nameIndex = classString.indexOf(className);
+    if (nameIndex == -1) {
+        classString += ' ' + className;
+    }
+    else {
+        classString = classString.substr(0, nameIndex) + classString.substr(nameIndex+className.length);
+    }
+    element.className = classString;
+}
+
+document.getElementsByClassName('art').addEventListener('click', function() {
+    toggleClass(this, 'art-mod');
+});
